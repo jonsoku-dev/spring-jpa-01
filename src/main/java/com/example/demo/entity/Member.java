e89c7de13ff8fe93a0126f21f52bf0233ce1dc48
jongseok-lee <jongseok.lee@linecorp.com>
2020-07-27 17:54:22 +0900

    first commit

---
+++ src/main/java/com/example/demo/entity/Member.java
@@ -0,0 +1,22 @@
+package com.example.demo.entity;
+
+import lombok.Getter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+public class Member {
+    @Id @GeneratedValue
+    private Long id;
+    private String username;
+
+    public Member() {
+    }
+
+    public Member(String username){
+        this.username = username;
+    }
+}
