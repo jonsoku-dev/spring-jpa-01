c06a6fbb34c5cb8d779304efb08fa73e9c7296e5
jongseok-lee <jongseok.lee@linecorp.com>
2020-07-27 18:05:27 +0900

    팀 entity 생성

--- src/main/java/com/example/demo/entity/Member.java
+++ src/main/java/com/example/demo/entity/Member.java
@@ -1,11 +1,16 @@
 package com.example.demo.entity;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import javax.persistence.*;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"}) // 연관관계 필드는 ToString 하지 않는 것이 좋다.
 public class Member {
     @Id @GeneratedValue
     @Column(name = "member_id")
@@ -18,10 +23,11 @@ public class Member {
     @JoinColumn(name="team_id")
     private Team team;
 
-    public Member() {
-    }
+    public Member(String username) {  this.username = username; }
 
-    public Member(String username){
-        this.username = username;
+    // 자신만 바꾸지않고, 반대쪽도 바꿔야하니 아래의 로직을 넣는다.
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
     }
 }
--- src/main/java/com/example/demo/entity/Team.java
+++ src/main/java/com/example/demo/entity/Team.java
@@ -1,10 +1,18 @@
 package com.example.demo.entity;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "name"})
 public class Team {
     @Id @GeneratedValue
     @Column(name="team_id")
@@ -14,6 +22,8 @@ public class Team {
     @OneToMany(mappedBy = "team")
     private List<Member> members = new ArrayList<>();
 
-    public Team() {
+    public Team(String name) {
+        this.name = name;
     }
+
 }
